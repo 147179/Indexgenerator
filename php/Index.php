00e9aeb69a316c87e24f466698a6533321325cb8
Sam <147179@github.com>
2016-06-23 21:12:18 +0200

    q

--- php/Index.php
+++ php/Index.php
@@ -3,6 +3,6 @@
 /*
  * Opdracht:
  * Teller maken die telt hoe vaak een woord terug komt.
- * Selectie van meest populaire woorden.
+ * Daarvan selectie van meest populaire woorden.
  * Van die selectie de lidwoorden eruit filteren.
  */
